35c9a31fa10d7c9d505b4e456e5af05df7f48251
唐晟庆 2024051604041 <2052448030@qq.com>
2026-01-19 13:49:51 +0800

    test

--- doc/StudentCourse.cppm
+++ doc/StudentCourse.cppm
@@ -2,10 +2,10 @@
 
 
 export module studentCourse;
-export import :student;
+
 export import :teacher;
 export import :course;
-
+export import :student;
 
 export import  :registrar;
 
--- doc/main.cpp
+++ doc/main.cpp
@@ -3,7 +3,7 @@
 import studentCourse;
 import std;
 int main()
-{/ Module
+{
             // File: main.cpp   Version: 0.1.0   License: AGPLv3
             // Created: pangwenhao      2026-01-17 16:03:13
             // Description:
